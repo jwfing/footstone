7f5859242ea52b6f9cf10085da3b37a90daf4176
junwen <jwfing@gmail.com>
2015-01-12 23:23:12 +0800

    initialize

---
+++ sample/java/footstone-app/src/main/java/org/jwfing/footstone/app/App.java
@@ -0,0 +1,13 @@
+package org.jwfing.footstone.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
